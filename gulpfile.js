a01f347fb2bb8b4488892f75f307ce7f84890baf
mobystudent <mobystudent@gmail.com>
2020-09-04 14:58:55 +0300

    Start

---
+++ gulpfile.js
@@ -0,0 +1,136 @@
+'use strict';
+
+const gulp = require('gulp'),
+	del = require('del'),
+	pug = require('gulp-pug'),
+	imagemin = require('gulp-imagemin'),
+	autoprefixer = require('gulp-autoprefixer'),
+	sass = require('gulp-sass'),
+	groupcmq = require('gulp-group-css-media-queries'),
+	sourcemaps = require('gulp-sourcemaps'),
+	browserSync = require('browser-sync').create();
+
+/* settings */
+const dirBuild = 'build',
+	dirSrc = 'src',
+	path = {
+		build: {
+			html: dirBuild,
+			css: dirBuild,
+			pug: dirSrc + '/template',
+			fonts: dirBuild + '/fonts',
+			favicon: dirBuild + '/favicon',
+			img: dirBuild + '/img',
+			js: dirBuild + '/js'
+		},
+		src: {
+			html: dirSrc + '/template/*.html',
+			css: dirSrc + '/css/style.scss',
+			pug: dirSrc + '/pug/**/*.pug',
+			fonts: dirSrc + '/fonts/**/*',
+			favicon: dirSrc + '/favicon/*',
+			img: dirSrc + '/img/**/*',
+			js: dirSrc + '/js/script.js'
+		},
+		watch: {
+			html: dirSrc + '/template/**/*.html',
+			css: dirSrc + '/css/**/*.scss',
+			pug: dirSrc + '/pug/**/*.pug',
+			fonts: dirSrc + '/fonts/**/*',
+			favicon: dirSrc + '/favicon/*',
+			img: dirSrc + '/img/**/*',
+			js: dirSrc + '/js/**/*.js'
+		}
+	};
+
+/* clear build dir */
+function clean() {
+	return del(dirBuild + '/**');
+}
+
+/* coplite sass */
+function gulpSass() {
+	return gulp.src(path.src.css)
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			outputStyle: 'compressed'
+		})
+		.on('error', sass.logError))
+		.pipe(groupcmq())
+		.pipe(autoprefixer({
+			overrideBrowserslist: ['> 0.4%, last 4 versions, firefox >= 52, edge >= 16, ie >= 11, safari >=10']
+		}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(path.build.css));
+}
+
+/* copy fonts in build dir */
+function gulpFonts() {
+	return gulp.src(path.src.fonts)
+		.pipe(gulp.dest(path.build.fonts));
+}
+
+
+/* coplite pug files in html folder */
+function gulpPug() {
+	return gulp.src(path.src.pug)
+		.pipe(pug({
+			pretty: true
+		}))
+		.pipe(gulp.dest(path.build.pug));
+}
+
+/* copy html in build dir */
+function gulpHTML() {
+	return gulp.src(path.src.html)
+		.pipe(gulp.dest(path.build.html));
+}
+
+/* optimize images */
+function gulpImages() {
+	return gulp.src(path.src.img)
+		.pipe(imagemin([
+			imagemin.mozjpeg({quality: 90, progressive: true}),
+			imagemin.optipng(),
+			imagemin.svgo()
+		]))
+		.pipe(gulp.dest(path.build.img));
+}
+
+/* copy favicon in build dir */
+function gulpFavicon() {
+	return gulp.src(path.src.favicon)
+		.pipe(gulp.dest(path.build.favicon));
+}
+
+/* compile js bundle */
+function gulpJS() {
+	return gulp.src(path.src.js)
+		.pipe(gulp.dest(path.build.js));
+}
+
+/* watch src files and show changes in browser */
+function gulpWatch() {
+	browserSync.init({
+		server: './'+dirBuild
+	});
+
+	gulp.watch(path.watch.css, gulp.series(gulpSass));
+	gulp.watch(path.watch.fonts, gulp.series(gulpFonts));
+	gulp.watch(path.watch.pug, gulp.series(gulpPug));
+	gulp.watch(path.watch.html, gulp.series(gulpHTML));
+	gulp.watch(path.watch.img, gulp.series(gulpImages));
+	gulp.watch(path.watch.favicon, gulp.series(gulpFavicon));
+	gulp.watch(path.watch.js, gulp.series(gulpJS));
+}
+
+const dev = gulp.series(clean, gulp.parallel(gulpSass, gulpHTML, gulpPug, gulpFonts, gulpFavicon, gulpJS)),
+	build = gulp.series(clean, gulp.parallel(gulpSass, gulpHTML, gulpFonts, gulpFavicon, gulpJS, gulpImages));
+
+exports.default = build;
+exports.watch = gulp.series(build, gulpWatch);
+exports.dev = gulp.series(dev, gulpWatch);
+exports.clean = clean;
+exports.js = gulpJS;
+exports.img = gulpImages;
+exports.fonts = gulpFonts;
